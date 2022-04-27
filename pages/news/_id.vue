93eb61dc21d792617a1db02d0c498cde43df1cbf
nanami <kom_nana5102@icloud.com>
2022-04-27 13:15:59 +0900

    :sparkles: added Content

---
+++ pages/news/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <spearly-content :id="current">
+    <template v-slot="content">
+      <header>
+        <h1>{{ content.values.title }}</h1>
+      </header>
+      <div v-html="content.values.description" />
+      <img :src="content.values.image" />
+      {{ $dayjs(content.values.date).format(`YYYY-MM-DD`) }}
+    </template>
+  </spearly-content>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+
+  computed: {
+    current() {
+      return this.$route.params.id
+    },
+  },
+})
+</script>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,15 +1,14 @@
 <template>
-  <!-- <spearly-content-list id="news">
+  <spearly-content-list id="news">
     <template v-slot="item">
-      <nuxt-link :to="`/${item.content.attributes.publicUid}`">
+      <nuxt-link :to="`/news/${item.content.attributes.publicUid}`">
         {{ item.content.values.title }}
         <img :src="item.content.values.image" />
         {{ item.content.values.description }}
         {{ $dayjs(item.content.values.date).format(`YYYY-MM-DD`) }}
       </nuxt-link>
     </template>
-  </spearly-content-list> -->
-  <nuxt-link to="/news">Home page</nuxt-link>
+  </spearly-content-list>
 </template>
 
 <script lang="ts">
